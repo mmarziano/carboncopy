85be2325661bbb17b9095f51f0cc86910c7d8b8a
Monica Marziano <mmarziano1@gmail.com>
2019-10-28 09:01:57 -0400

    add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index' 
+
+  devise_for :users do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+ end
 end
