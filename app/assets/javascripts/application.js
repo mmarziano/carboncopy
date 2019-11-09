17b4473fccdf560c5782af7250e804b3eb78877f
Monica Marziano <mmarziano1@gmail.com>
2019-11-08 19:44:25 -0500

    finish preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,7 @@ function attachListeners() {
         e.preventDefault();
         start();
     })
+
     let search = document.querySelector('#submit-search');
     search.addEventListener('click', function(e) {
        e.preventDefault();
@@ -38,17 +39,20 @@ function attachListeners() {
        div.innerHTML = "";
        getOrganizations();
     })
+ 
     let reset = document.querySelector('#reset');
     reset.addEventListener('click', function(e){
         e.preventDefault();
         restart();
     })
+
     let newOrg = document.querySelector('#create_new');
     newOrg.addEventListener('click', function(e){
         e.preventDefault();
         createOrganization();
     })    
-}
+  
+};
 
 function showCard() {
     let flipcard = document.querySelector('.flip-card')
@@ -191,7 +195,7 @@ function hideReceiptTypeChoice() {
     div.classList.add('hidden');
 }
 
-function showResetReceipt(input) {
+function showResetReceipt(input, receipt) {
     let org = () => input;
     let reset = document.querySelector('#reset-receipt');
     reset.classList.remove('hidden');
@@ -202,9 +206,9 @@ function showResetReceipt(input) {
         hideNextMultiple();
         hidePreviousMultiple();
         resetStep();
-        selectType(org());
+        selectType(org());  
     })
-    
+ 
 }
 
 function hideResetReceipt() {
@@ -238,44 +242,6 @@ function hideOneCategoryReceiptFormElements() {
     elements.push(receivedBy);
     for (i = 0; i < elements.length; i++) {
         elements[i].classList.add('hidden')
-    }
-    return elements;
-}
-
-function hideMultiCategoryReceiptFormElements() {
-    let elements = [];
-    let name = document.querySelector('#name-group');
-    elements.push(name);
-    let email = document.querySelector('#email-group');
-    elements.push(email);
-    let phone = document.querySelector('#phone-group');
-    elements.push(phone);
-    let secondName = document.querySelector('#secondary-name-group');
-    elements.push(secondName);
-    let id = document.querySelector('#secondary-id-group');
-    elements.push(id);
-    let cat1 = document.querySelector('#category-label-1-group');
-    elements.push(cat1);
-    let amt1 = document.querySelector('#category-amt-1-group');
-    elements.push(amt1);
-    let cat2 = document.querySelector('#category-label-2-group');
-    elements.push(cat2);
-    let amt2 = document.querySelector('#category-amt-2-group');
-    elements.push(amt2);
-    let cat3 = document.querySelector('#category-label-3-group');
-    elements.push(cat3);
-    let amt3 = document.querySelector('#category-amt-3-group');
-    elements.push(amt3);
-    let method = document.querySelector('#payment-method-group');
-    elements.push(method);
-    let methodNote = document.querySelector('#payment-method-note-group');
-    elements.push(methodNote);
-    let notes = document.querySelector('#notes-group');
-    elements.push(notes);
-    let receivedBy = document.querySelector('#received-by-group');
-    elements.push(receivedBy);
-    for (i = 0; i < elements.length; i++) {
-        elements[i].classList.add('hidden');
     }
     return elements;
 }
@@ -374,6 +340,7 @@ function generateTableHead(tbl, data) {
             e.preventDefault();
             authenticate(data[i]);
         });
+      
         let row = tbody.insertRow();
         for (let key in data[i]) {
             let cell = row.insertCell();
@@ -406,11 +373,12 @@ function generateTableHead(tbl, data) {
         e.preventDefault();
         let userPin = document.querySelector('#pin').value;
         if (orgPin() === userPin.toString()) {
-            selectType(org());
+            startSingleReceipt(org());
         } else {
             alert("Wrong!")
         }
     }); 
+
   }
 
   function resetStep() {
@@ -418,33 +386,7 @@ function generateTableHead(tbl, data) {
       return step;
   }
 
-  function selectType(org) {
-    hideCard();
-    hidePin();
-    hideResetLink();
-    hideCreateOrgForm();
-    hideSearch();  
-    hideError();
-    hideResetReceipt();
-    hideReceiptForm();
-    hideOneCategoryReceiptFormElements();
-    hideMultiCategoryReceiptFormElements();
-    showReceiptTypeChoice();
-    let single = document.querySelector('#single-category');
-    single.addEventListener('click', function(e){
-        e.preventDefault();
-        hideOneCategoryReceiptFormElements();
-        startSingleReceipt(org, resetStep());
-    });
-    let multiple = document.querySelector('#multiple-category');
-    multiple.addEventListener('click', function(e){
-        e.preventDefault();
-        startMultipleReceipt(org, resetStep());
-    });
-  }
-
-  function startSingleReceipt(org, start) {
-    hideReceiptTypeChoice();  
+  function startSingleReceipt(org) {  
     let receipt = {};
     let getReceipt = () => {return receipt};
     showReceiptForm();   
@@ -454,8 +396,8 @@ function generateTableHead(tbl, data) {
     hideCreateOrgForm();
     hideSearch();  
     hideError();
-    showResetReceipt(org);
-    let step = start;
+    showResetReceipt(org, receipt);
+    let step = 0;
     let name = document.querySelector('#name-group');
     name.classList.remove('hidden');
     let next = document.querySelector('#next');
@@ -510,76 +452,7 @@ function generateTableHead(tbl, data) {
         e.preventDefault();
         previewReceipt(org, getReceipt());
     })
-  }
 
-  function startMultipleReceipt(org, start) {
-    hideReceiptTypeChoice();  
-    let receipt = {};
-    let getReceipt = () => receipt;
-    showReceiptForm();   
-    hideCard();
-    hidePin();
-    hideResetLink();
-    hideCreateOrgForm();
-    hideSearch();  
-    hideError();
-    showResetReceipt(org);
-    
-    let step = start;
-    let name = document.querySelector('#name-group');
-    name.classList.remove('hidden');
-    let next = document.querySelector('#next-multiple');
-    next.classList.remove('hidden');
-    let previous = document.querySelector('#previous-multiple');
-    previous.classList.remove('hidden');
-    next.addEventListener('click', function(e){
-        e.preventDefault();
-        if (step > 14) {
-            let button = document.querySelector('#create_receipt_submit');
-            let next = document.querySelector('#next');
-            next.classList.add('hidden')
-            button.classList.remove('hidden')
-        } else {
-            previous.classList.remove('hidden'); 
-            let elements = hideMultiCategoryReceiptFormElements();
-            let k = elements[step].getAttribute('id').split('-');
-            k.pop();
-            let key = k.join('_')
-            receipt[`${key}`] = elements[step].children[1].value;
-            if (step !== 14) {
-                elements[step+1].classList.remove('hidden')
-                step++;
-            } else {
-                let button = document.querySelector('#create_receipt_submit');
-                let next = document.querySelector('#next');
-                next.classList.add('hidden')
-                button.classList.remove('hidden')
-            }
-        }
-    })
-    previous.addEventListener('click', function(e){
-        e.preventDefault();
-        if (step < 0) {
-            previous.classList.add('hidden');
-            step = 0;    
-        } else if (step > 27) {
-            let button = document.querySelector('#create_receipt_submit');
-            let next = document.querySelector('#next');
-            next.classList.remove('hidden')
-            button.classList.add('hidden')
-            step--;
-        } else {
-            previous.classList.remove('hidden'); 
-            let elements = hideMultiCategoryReceiptFormElements();
-            elements[step].classList.remove('hidden')
-            step--;
-        }  
-    })
-    let button = document.querySelector('#create_receipt_submit');
-    button.addEventListener('click', function(e){
-        e.preventDefault();
-        previewReceipt(org, getReceipt());
-    })
   }
 
 
@@ -592,24 +465,6 @@ class  Receipt {
         this.secondary_id = obj.secondary_id;
         this.category_label_1 = obj.category_label_1;
         this.category_amt_1 = obj.category_amt_1;
-        this.category_label_2 = obj.category_label_2;
-        this.category_amt_2 = obj.category_amt_2;
-        this.category_label_3 = obj.category_label_3;
-        this.category_amt_3 = obj.category_amt_3;
-        this.category_label_4 = obj.category_label_4;
-        this.category_amt_4 = obj.category_amt_4;
-        this.category_label_5 = obj.category_label_5;
-        this.category_amt_5 = obj.category_amt_5;
-        this.category_label_6 = obj.category_label_6;
-        this.category_amt_6 = obj.category_amt_6;
-        this.category_label_7 = obj.category_label_7;
-        this.category_amt_7 = obj.category_amt_7;
-        this.category_label_8 = obj.category_label_8;
-        this.category_amt_8 = obj.category_amt_8;
-        this.category_label_9 = obj.category_label_9;
-        this.category_amt_9 = obj.category_amt_9;
-        this.category_label_10 = obj.category_label_10;
-        this.category_amt_10 = obj.category_amt_10;
         this.payment_method = obj.payment_method;
         this.payment_method_note = obj.payment_method_note;
         this.notes = obj.notes;
@@ -619,18 +474,6 @@ class  Receipt {
 
 }
   function previewReceipt(org, receipt) {
-    if (receipt.category_label_2 === undefined) {
-        receipt.category_label_2 = "None specified";
-    }  
-    if (receipt.category_label_3 === undefined) {
-        receipt.category_label_3 = "None specified";
-    } 
-    if (receipt.category_amt_2 === undefined) {
-        receipt.category_amt_2 = 0.00;
-    }  
-    if (receipt.category_amt_3 === undefined) {
-        receipt.category_amt_3 = 0.00;
-    } 
     hideCard();
     hidePin();
     hideResetLink();
@@ -640,7 +483,6 @@ class  Receipt {
     hideResetReceipt();
     hideReceiptForm();
     hideOneCategoryReceiptFormElements();
-    hideMultiCategoryReceiptFormElements();
     receipt['receipt_date'] = new Date(Date.now()).toLocaleString();
     let preview = document.querySelector('#preview-receipt');
     preview.classList.remove('hidden');
@@ -706,34 +548,6 @@ class  Receipt {
     amount1
     amt1span.innerText = `$${amount1}`;
     amt1.append(amt1span);
-    let cat2 = document.createElement('h4');
-    cat2.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
-    cat2.innerText = `2nd Payment Category: `
-    let cat2span = span();
-    cat2span.innerText = receipt.category_label_2;
-    cat2.append(cat2span);
-    let amt2 = document.createElement('h4');
-    amt2.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
-    amt2.innerText = `2nd Payment Amount:`
-    let amt2span = span();
-    let amount2 = parseFloat(receipt.category_amt_2).toFixed(2)
-    amount2
-    amt2span.innerText = `$${amount2}`;
-    amt2.append(amt2span);
-    let cat3 = document.createElement('h4');
-    cat3.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
-    cat3.innerText = `3rd Payment Category: `
-    let cat3span = span();
-    cat3span.innerText = receipt.category_label_3;
-    cat3.append(cat3span);
-    let amt3 = document.createElement('h4');
-    amt3.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
-    amt3.innerText = `3rd Payment Amount:`
-    let amt3span = span();
-    let amount3 = parseFloat(receipt.category_amt_3).toFixed(2);
-    amount3
-    amt3span.innerText = `$${amount3}`;
-    amt3.append(amt3span);
     let method = document.createElement('h4');
     method.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
     method.innerText = `Payment Method:`
@@ -754,7 +568,7 @@ class  Receipt {
     notes.append(notesspan);
     let footer = document.createElement('div');
     footer.setAttribute('class', "summary");
-    let sum = parseFloat(amount1) + parseFloat(amount2) + parseFloat(amount3);
+    let sum = parseFloat(amount1);
     sum
     let receivedBy = document.createElement('h4');
     receivedBy.setAttribute('style', "color:rgb(240, 8, 143); padding: 10px");
@@ -770,9 +584,20 @@ class  Receipt {
     date.append(datespan);
     let total = document.createElement('h1');
     total.setAttribute('class', 'total');
+    total.setAttribute('style', 'float:right;')
     total.innerText = "Total: "
     total.append(` $${sum}`);
     footer.append(total)
+    let button = document.createElement('button');
+    button.setAttribute('class', 'btn btn-success');
+    button.setAttribute('id', 'save-receipt')
+    button.innerText = "Record Receipt";
+    button.addEventListener('click', function(e){
+        e.preventDefault();
+        saveReceipt();
+    })
+    footer.append(button);
+
     preview.appendChild(body);
     body.appendChild(recipient);
     body.appendChild(email);
@@ -781,10 +606,6 @@ class  Receipt {
     body.appendChild(id);
     body.appendChild(cat1);
     body.appendChild(amt1);
-    body.appendChild(cat2);
-    body.appendChild(amt2);
-    body.appendChild(cat3);
-    body.appendChild(amt3);
     body.appendChild(method);
     body.appendChild(methodNotes);
     body.appendChild(notes);
@@ -794,7 +615,9 @@ class  Receipt {
     
   }
 
-
+function saveReceipt() {
+    alert('heres')
+}
 
 
   
--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -7,9 +7,20 @@ class ReceiptsController < ApplicationController
     end 
 
     def create
+        raise.params.inspect
+        receipt = Receipt.new(receipt_params)
+
+        if receipt.save
+            render json: organization
+        else 
+            flash[:alert] = receipt.errors.full_messages if receipt.errors.any?
+            redirect_to '/'
+        end
     end 
 
     def show
+        receipt = Receipt.find(params[:id])
+        render json: receipt
     end
 
     def edit
@@ -21,5 +32,9 @@ class ReceiptsController < ApplicationController
     def destroy
     end
 
+    private 
 
+    def receipt_params
+        params.require(:receipt).permit(:name, :email, :phone, :secondary_name, :secondary_id, :category_label_1, :category_amt_1, :payment_method, :payment_method_notes, :notes, :received_by, :receipt_date)
+    end 
 end
