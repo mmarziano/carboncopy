d28fefe85c4bb4ddd552922920ce71ea99bba676
Monica Marziano <mmarziano1@gmail.com>
2019-11-25 19:14:28 -0500

    create org form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,14 +33,6 @@ function attachListeners() {
         quickStart.classList.add('hidden')
         Search();
     })
-
-    // let search = document.querySelector('#submit-search');
-    // search.addEventListener('click', function(e) {
-    //    e.preventDefault();
-    //    let div = document.querySelector('#search-results');
-    //    div.innerHTML = "";
-    //    getOrganizations();
-    // })
  
     let resetLink = document.querySelector('#reset');
     resetLink.addEventListener('click', function(e){
@@ -67,9 +59,31 @@ function attachListeners() {
 };
 
 function Search(){
+    let create = document.createElement('a');
+    create.id = "create_new";
+    create.style = "float:right; margin-right: 10px";
+    create.href = '/';
+    create.innerHTML = "Create New"
+    create.addEventListener('click', function(e){
+        e.preventDefault();
+        hideResults();
+        hideSearch();
+        createOrgForm();
+    })
+    let reset = document.createElement('a');
+    reset.id = "reset";
+    reset.style = "float: right";
+    reset.href = '/';
+    reset.innerHTML = "Reset";
+    reset.addEventListener('click', function(e){
+        e.preventDefault();
+    })
+
     let form = document.querySelector('#org-search-form');
     let h3 = document.createElement('h3');
     h3.innerHTML = "Search for Business/Organization"
+    form.appendChild(reset);
+    form.appendChild(create);
     form.appendChild(h3);
 
     let f = document.createElement("form");
@@ -104,30 +118,146 @@ function Search(){
     form.appendChild(f)
 };
 
-// function hideSearch() {
-//     let orgSearch = document.querySelector('#org-search');
-//     orgSearch.classList.add('hidden');
-// }
+function createOrgForm() {
+    let form = document.querySelector('#create_org_form');
+    let h3 = document.createElement('h3');
+    h3.innerHTML = "Create New Organization"
+    form.appendChild(h3);
 
-// function showSearch() {
-//     let orgSearch = document.querySelector('#org-search');
-//     orgSearch.classList.remove('hidden');
-// }
+    let f = document.createElement("form");
+    f.setAttribute('method',"post");
+    f.setAttribute('action',"/organizations");
+    
+    //creates 'Name' input element
+    let iName = document.createElement("input");
+    iName.type = "text";
+    iName.name = "name";
+    iName.id = "name";
+    iName.setAttribute('class', 'form-control');
+    iName.placeholder = "Organization name...";
+    iName.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'Address' input element
+    let iAddress = document.createElement("input");
+    iAddress.type = "text";
+    iAddress.name = "address";
+    iAddress.id = "address";
+    iAddress.setAttribute('class', 'form-control');
+    iAddress.placeholder = "Street Address"
+    iAddress.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'City' input element
+    let iCity = document.createElement("input");
+    iCity.type = "text";
+    iCity.name = "city";
+    iCity.id = "city";
+    iCity.setAttribute('class', 'form-control');
+    iCity.placeholder = "City"
+    iCity.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'State' input element
+    let iState = document.createElement("input");
+    iState.type = "text";
+    iState.name = "state";
+    iState.id = "state";
+    iState.setAttribute('class', 'form-control');
+    iState.placeholder = "State"
+    iState.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+    
+    //creates 'Zipcode' input element
+    let iZipcode = document.createElement("input");
+    iZipcode.type = "text";
+    iZipcode.name = "zipcode";
+    iZipcode.id = "zipcode";
+    iZipcode.setAttribute('class', 'form-control');
+    iZipcode.placeholder = "Zipcode"
+    iZipcode.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'Phone' input element
+    let iPhone = document.createElement("input");
+    iPhone.type = "text";
+    iPhone.name = "phone";
+    iPhone.id = "phone";
+    iPhone.setAttribute('class', 'form-control');
+    iPhone.placeholder = "Phone"
+    iPhone.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'Billing Email' input element
+    let iBillingEmail = document.createElement("input");
+    iBillingEmail.type = "text";
+    iBillingEmail.name = "billing_email";
+    iBillingEmail.id = "billing_email";
+    iBillingEmail.setAttribute('class', 'form-control');
+    iBillingEmail.placeholder = "Billing Email"
+    iBillingEmail.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'Audit Email' input element
+    let iAuditEmail = document.createElement("input");
+    iAuditEmail.type = "text";
+    iAuditEmail.name = "audit_email";
+    iAuditEmail.id = "audit_email";
+    iAuditEmail.setAttribute('class', 'form-control');
+    iAuditEmail.placeholder = "Audit Email"
+    iAuditEmail.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates 'Pin' input element
+    let iPin = document.createElement("input");
+    iPin.type = "text";
+    iPin.name = "pin";
+    iPin.id = "pin";
+    iPin.setAttribute('class', 'form-control');
+    iPin.placeholder = "Pin/Password"
+    iPin.style = "padding: 20px; border: none; border-bottom: 2px solid rgb(240, 8, 143); width: 100%; margin-bottom: 20px"
+
+    //creates submit button
+    var s = document.createElement("input");
+    s.type = "submit";
+    s.setAttribute('class', "btn marz-button")
+    s.value = "Create Organization";
+    s.addEventListener('click', function(e){
+        e.preventDefault();
+        alert('here')
+        }); 
+    
+    // add all elements to the form
+    f.appendChild(iName);
+    f.appendChild(iAddress);
+    f.appendChild(iCity);
+    f.appendChild(iState);
+    f.appendChild(iZipcode);
+    f.appendChild(iPhone);
+    f.appendChild(iBillingEmail);
+    f.appendChild(iAuditEmail);
+    f.appendChild(iPin);
+    f.appendChild(s);
+
+    form.appendChild(f)
+}
+
+function hideSearch() {
+    let orgSearch = document.querySelector('#org-search-form');
+    orgSearch.classList.add('hidden');
+}
+
+function showSearch() {
+    let orgSearch = document.querySelector('#org-search-form');
+    orgSearch.classList.remove('hidden');
+}
 
 // function clearSearch() {
 //     let orgSearch = document.querySelector('#org-search');
 //     orgSearch.children[2].name.value = '';
 // }
 
-function hideCreateOrgForm() {
-    let newOrg = document.querySelector('#create_new');
-    newOrg.classList.add('hidden');
-}
+// function hideCreateOrgForm() {
+//     let newOrg = document.querySelector('#create_new');
+//     newOrg.classList.add('hidden');
+// }
 
-function showCreateOrgForm() {
-    let newOrg = document.querySelector('#create_new');
-    newOrg.classList.remove('hidden');
-}
+// function showCreateOrgForm() {
+//     let newOrg = document.querySelector('#create_new');
+//     newOrg.classList.remove('hidden');
+// }
 
 function hideResetLink() {
     let reset = document.querySelector('#reset');
